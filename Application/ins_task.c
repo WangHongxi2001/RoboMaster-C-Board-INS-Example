590c45ba84040147ed541f64181bf42ee20847dd
NeoZng <60593302+NeoZng@users.noreply.github.com>
2022-07-19 01:21:25 +0800

    fix a little bug

--- Application/ins_task.c
+++ Application/ins_task.c
@@ -66,24 +66,34 @@ void INS_Task(void)
         INS.Gyro[X] = BMI088.Gyro[X];
         INS.Gyro[Y] = BMI088.Gyro[Y];
         INS.Gyro[Z] = BMI088.Gyro[Z];
+
+        // demo function,用于修正安装误差,可以不管,本demo暂时没用
         IMU_Param_Correction(&IMU_Param, INS.Gyro, INS.Accel);
+
+        // 计算重力加速度矢量和b系的XY两轴的夹角,可用作功能扩展,本demo暂时没用
         INS.atanxz = -atan2f(INS.Accel[X], INS.Accel[Z]) * 180 / PI;
         INS.atanyz = atan2f(INS.Accel[Y], INS.Accel[Z]) * 180 / PI;
 
+        // 核心函数,EKF更新四元数
         IMU_QuaternionEKF_Update(INS.Gyro[X], INS.Gyro[Y], INS.Gyro[Z], INS.Accel[X], INS.Accel[Y], INS.Accel[Z], dt);
 
         memcpy(INS.q, QEKF_INS.q, sizeof(QEKF_INS.q));
 
+        // 转换到大地/绝对坐标系
         BodyFrameToEarthFrame(xb, INS.xn, INS.q);
         BodyFrameToEarthFrame(yb, INS.yn, INS.q);
         BodyFrameToEarthFrame(zb, INS.zn, INS.q);
 
+        // 将重力从绝对坐标系n转换到机体系b,随后根据加速度计数据计算运动加速度
         float gravity_b[3];
         EarthFrameToBodyFrame(gravity, gravity_b, INS.q);
-        for (uint8_t i = 0; i < 3; i++)
+        for (uint8_t i = 0; i < 3; i++) // 同样过一个低通滤波
+        {
             INS.MotionAccel_b[i] = (INS.Accel[i] - gravity_b[i]) * dt / (INS.AccelLPF + dt) + INS.MotionAccel_b[i] * INS.AccelLPF / (INS.AccelLPF + dt);
-        BodyFrameToEarthFrame(INS.MotionAccel_b, INS.MotionAccel_n, INS.q);
+        }
+        BodyFrameToEarthFrame(INS.MotionAccel_b, INS.MotionAccel_n, INS.q); // 转换回绝对系n
 
+        // 获取最终数据
         INS.Yaw = QEKF_INS.Yaw;
         INS.Pitch = QEKF_INS.Pitch;
         INS.Roll = QEKF_INS.Roll;
@@ -104,6 +114,135 @@ void INS_Task(void)
 
     count++;
 }
+
+
+/**
+ * @brief          Transform 3dvector from BodyFrame to EarthFrame
+ * @param[1]       vector in BodyFrame
+ * @param[2]       vector in EarthFrame
+ * @param[3]       quaternion
+ */
+void BodyFrameToEarthFrame(const float *vecBF, float *vecEF, float *q)
+{
+    vecEF[0] = 2.0f * ((0.5f - q[2] * q[2] - q[3] * q[3]) * vecBF[0] +
+                       (q[1] * q[2] - q[0] * q[3]) * vecBF[1] +
+                       (q[1] * q[3] + q[0] * q[2]) * vecBF[2]);
+
+    vecEF[1] = 2.0f * ((q[1] * q[2] + q[0] * q[3]) * vecBF[0] +
+                       (0.5f - q[1] * q[1] - q[3] * q[3]) * vecBF[1] +
+                       (q[2] * q[3] - q[0] * q[1]) * vecBF[2]);
+
+    vecEF[2] = 2.0f * ((q[1] * q[3] - q[0] * q[2]) * vecBF[0] +
+                       (q[2] * q[3] + q[0] * q[1]) * vecBF[1] +
+                       (0.5f - q[1] * q[1] - q[2] * q[2]) * vecBF[2]);
+}
+
+/**
+ * @brief          Transform 3dvector from EarthFrame to BodyFrame
+ * @param[1]       vector in EarthFrame
+ * @param[2]       vector in BodyFrame
+ * @param[3]       quaternion
+ */
+void EarthFrameToBodyFrame(const float *vecEF, float *vecBF, float *q)
+{
+    vecBF[0] = 2.0f * ((0.5f - q[2] * q[2] - q[3] * q[3]) * vecEF[0] +
+                       (q[1] * q[2] + q[0] * q[3]) * vecEF[1] +
+                       (q[1] * q[3] - q[0] * q[2]) * vecEF[2]);
+
+    vecBF[1] = 2.0f * ((q[1] * q[2] - q[0] * q[3]) * vecEF[0] +
+                       (0.5f - q[1] * q[1] - q[3] * q[3]) * vecEF[1] +
+                       (q[2] * q[3] + q[0] * q[1]) * vecEF[2]);
+
+    vecBF[2] = 2.0f * ((q[1] * q[3] + q[0] * q[2]) * vecEF[0] +
+                       (q[2] * q[3] - q[0] * q[1]) * vecEF[1] +
+                       (0.5f - q[1] * q[1] - q[2] * q[2]) * vecEF[2]);
+}
+
+/**
+ * @brief reserved.用于修正IMU安装误差,即陀螺仪轴和云台轴的offset
+ *
+ *
+ * @param param 有误差的机体位姿
+ * @param gyro  角速度,稍后修正并转换到绝对系
+ * @param accel 加速度,稍后修正并转换到绝对系
+ */
+static void IMU_Param_Correction(IMU_Param_t *param, float gyro[3], float accel[3])
+{
+    static float lastYawOffset, lastPitchOffset, lastRollOffset;
+    static float c_11, c_12, c_13, c_21, c_22, c_23, c_31, c_32, c_33;
+    float cosPitch, cosYaw, cosRoll, sinPitch, sinYaw, sinRoll;
+
+    // 当初始化即flag==true或IMU相对云台机械位置等出现大的位移/姿态变化的时更新(如变形/伸展等)
+    if (fabsf(param->Yaw - lastYawOffset) > 0.001f ||
+        fabsf(param->Pitch - lastPitchOffset) > 0.001f ||
+        fabsf(param->Roll - lastRollOffset) > 0.001f || param->flag)
+    {
+        cosYaw = arm_cos_f32(param->Yaw / 57.295779513f);
+        cosPitch = arm_cos_f32(param->Pitch / 57.295779513f);
+        cosRoll = arm_cos_f32(param->Roll / 57.295779513f);
+        sinYaw = arm_sin_f32(param->Yaw / 57.295779513f);
+        sinPitch = arm_sin_f32(param->Pitch / 57.295779513f);
+        sinRoll = arm_sin_f32(param->Roll / 57.295779513f);
+
+        // 1.yaw(alpha) 2.pitch(beta) 3.roll(gamma)
+        c_11 = cosYaw * cosRoll + sinYaw * sinPitch * sinRoll;
+        c_12 = cosPitch * sinYaw;
+        c_13 = cosYaw * sinRoll - cosRoll * sinYaw * sinPitch;
+        c_21 = cosYaw * sinPitch * sinRoll - cosRoll * sinYaw;
+        c_22 = cosYaw * cosPitch;
+        c_23 = -sinYaw * sinRoll - cosYaw * cosRoll * sinPitch;
+        c_31 = -cosPitch * sinRoll;
+        c_32 = sinPitch;
+        c_33 = cosPitch * cosRoll;
+        param->flag = 0;
+    }
+    float gyro_temp[3];
+    for (uint8_t i = 0; i < 3; i++)
+        gyro_temp[i] = gyro[i] * param->scale[i];
+    // 计算b系下的角速度在n系下的投影
+    gyro[X] = c_11 * gyro_temp[X] +
+              c_12 * gyro_temp[Y] +
+              c_13 * gyro_temp[Z];
+    gyro[Y] = c_21 * gyro_temp[X] +
+              c_22 * gyro_temp[Y] +
+              c_23 * gyro_temp[Z];
+    gyro[Z] = c_31 * gyro_temp[X] +
+              c_32 * gyro_temp[Y] +
+              c_33 * gyro_temp[Z];
+
+    float accel_temp[3];
+    for (uint8_t i = 0; i < 3; i++)
+        accel_temp[i] = accel[i];
+    // 计算b系下的加速度在n系下的投影
+    accel[X] = c_11 * accel_temp[X] +
+               c_12 * accel_temp[Y] +
+               c_13 * accel_temp[Z];
+    accel[Y] = c_21 * accel_temp[X] +
+               c_22 * accel_temp[Y] +
+               c_23 * accel_temp[Z];
+    accel[Z] = c_31 * accel_temp[X] +
+               c_32 * accel_temp[Y] +
+               c_33 * accel_temp[Z];
+    // 更新offset
+    lastYawOffset = param->Yaw;
+    lastPitchOffset = param->Pitch;
+    lastRollOffset = param->Roll;
+}
+
+/**
+ * @brief 温度控制
+ * 
+ */
+void IMU_Temperature_Ctrl(void)
+{
+    PID_Calculate(&TempCtrl, BMI088.Temperature, RefTemp);
+
+    TIM_Set_PWM(&htim10, TIM_CHANNEL_1, float_constrain(float_rounding(TempCtrl.Output), 0, UINT32_MAX));
+}
+
+//------------------------------------functions below are not used in this demo-------------------------------------------------
+//----------------------------------you can read them for learning or programming-----------------------------------------------
+//----------------------------------they could also be helpful for further design-----------------------------------------------
 
 /**
  * @brief        Update quaternion
@@ -153,115 +292,4 @@ void EularAngleToQuaternion(float Yaw, float Pitch, float Roll, float *q)
     q[1] = sinPitch * cosRoll * cosYaw - cosPitch * sinRoll * sinYaw;
     q[2] = sinPitch * cosRoll * sinYaw + cosPitch * sinRoll * cosYaw;
     q[3] = cosPitch * cosRoll * sinYaw - sinPitch * sinRoll * cosYaw;
-}
-
-/**
- * @brief          Transform 3dvector from BodyFrame to EarthFrame
- * @param[1]       vector in BodyFrame
- * @param[2]       vector in EarthFrame
- * @param[3]       quaternion
- */
-void BodyFrameToEarthFrame(const float *vecBF, float *vecEF, float *q)
-{
-    vecEF[0] = 2.0f * ((0.5f - q[2] * q[2] - q[3] * q[3]) * vecBF[0] +
-                       (q[1] * q[2] - q[0] * q[3]) * vecBF[1] +
-                       (q[1] * q[3] + q[0] * q[2]) * vecBF[2]);
-
-    vecEF[1] = 2.0f * ((q[1] * q[2] + q[0] * q[3]) * vecBF[0] +
-                       (0.5f - q[1] * q[1] - q[3] * q[3]) * vecBF[1] +
-                       (q[2] * q[3] - q[0] * q[1]) * vecBF[2]);
-
-    vecEF[2] = 2.0f * ((q[1] * q[3] - q[0] * q[2]) * vecBF[0] +
-                       (q[2] * q[3] + q[0] * q[1]) * vecBF[1] +
-                       (0.5f - q[1] * q[1] - q[2] * q[2]) * vecBF[2]);
-}
-
-/**
- * @brief          Transform 3dvector from EarthFrame to BodyFrame
- * @param[1]       vector in EarthFrame
- * @param[2]       vector in BodyFrame
- * @param[3]       quaternion
- */
-void EarthFrameToBodyFrame(const float *vecEF, float *vecBF, float *q)
-{
-    vecBF[0] = 2.0f * ((0.5f - q[2] * q[2] - q[3] * q[3]) * vecEF[0] +
-                       (q[1] * q[2] + q[0] * q[3]) * vecEF[1] +
-                       (q[1] * q[3] - q[0] * q[2]) * vecEF[2]);
-
-    vecBF[1] = 2.0f * ((q[1] * q[2] - q[0] * q[3]) * vecEF[0] +
-                       (0.5f - q[1] * q[1] - q[3] * q[3]) * vecEF[1] +
-                       (q[2] * q[3] + q[0] * q[1]) * vecEF[2]);
-
-    vecBF[2] = 2.0f * ((q[1] * q[3] + q[0] * q[2]) * vecEF[0] +
-                       (q[2] * q[3] - q[0] * q[1]) * vecEF[1] +
-                       (0.5f - q[1] * q[1] - q[2] * q[2]) * vecEF[2]);
-}
-
-static void IMU_Param_Correction(IMU_Param_t *param, float gyro[3], float accel[3])
-{
-    static float lastYawOffset, lastPitchOffset, lastRollOffset;
-    static float c_11, c_12, c_13, c_21, c_22, c_23, c_31, c_32, c_33;
-    float cosPitch, cosYaw, cosRoll, sinPitch, sinYaw, sinRoll;
-
-    if (fabsf(param->Yaw - lastYawOffset) > 0.001f ||
-        fabsf(param->Pitch - lastPitchOffset) > 0.001f ||
-        fabsf(param->Roll - lastRollOffset) > 0.001f || param->flag)
-    {
-        cosYaw = arm_cos_f32(param->Yaw / 57.295779513f);
-        cosPitch = arm_cos_f32(param->Pitch / 57.295779513f);
-        cosRoll = arm_cos_f32(param->Roll / 57.295779513f);
-        sinYaw = arm_sin_f32(param->Yaw / 57.295779513f);
-        sinPitch = arm_sin_f32(param->Pitch / 57.295779513f);
-        sinRoll = arm_sin_f32(param->Roll / 57.295779513f);
-
-        // 1.yaw(alpha) 2.pitch(beta) 3.roll(gamma)
-        c_11 = cosYaw * cosRoll + sinYaw * sinPitch * sinRoll;
-        c_12 = cosPitch * sinYaw;
-        c_13 = cosYaw * sinRoll - cosRoll * sinYaw * sinPitch;
-        c_21 = cosYaw * sinPitch * sinRoll - cosRoll * sinYaw;
-        c_22 = cosYaw * cosPitch;
-        c_23 = -sinYaw * sinRoll - cosYaw * cosRoll * sinPitch;
-        c_31 = -cosPitch * sinRoll;
-        c_32 = sinPitch;
-        c_33 = cosPitch * cosRoll;
-        param->flag = 0;
-    }
-    float gyro_temp[3];
-    for (uint8_t i = 0; i < 3; i++)
-        gyro_temp[i] = gyro[i] * param->scale[i];
-
-    gyro[X] = c_11 * gyro_temp[X] +
-              c_12 * gyro_temp[Y] +
-              c_13 * gyro_temp[Z];
-    gyro[Y] = c_21 * gyro_temp[X] +
-              c_22 * gyro_temp[Y] +
-              c_23 * gyro_temp[Z];
-    gyro[Z] = c_31 * gyro_temp[X] +
-              c_32 * gyro_temp[Y] +
-              c_33 * gyro_temp[Z];
-
-    float accel_temp[3];
-    for (uint8_t i = 0; i < 3; i++)
-        accel_temp[i] = accel[i];
-
-    accel[X] = c_11 * accel_temp[X] +
-               c_12 * accel_temp[Y] +
-               c_13 * accel_temp[Z];
-    accel[Y] = c_21 * accel_temp[X] +
-               c_22 * accel_temp[Y] +
-               c_23 * accel_temp[Z];
-    accel[Z] = c_31 * accel_temp[X] +
-               c_32 * accel_temp[Y] +
-               c_33 * accel_temp[Z];
-
-    lastYawOffset = param->Yaw;
-    lastPitchOffset = param->Pitch;
-    lastRollOffset = param->Roll;
-}
-
-void IMU_Temperature_Ctrl(void)
-{
-    PID_Calculate(&TempCtrl, BMI088.Temperature, RefTemp);
-
-    TIM_Set_PWM(&htim10, TIM_CHANNEL_1, float_constrain(float_rounding(TempCtrl.Output), 0, UINT32_MAX));
 }
